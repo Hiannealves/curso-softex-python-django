9e63cd541b4df508660dea521514ef8690b6eac8
Hiannealves <hiannequeiroz@yahoo.com.br>
2025-09-01 16:05:56 -0300

    comecei um execício

---
+++ aula 09/ex04.py
@@ -0,0 +1,10 @@
+lista_numeros = []
+while True:
+    numeros = input(int("Digite um número: "))
+    if not numeros.isdigit():
+       print("Digte um número válido")
+
+    elif numeros == -1:
+        break   
+    
+    elif numeros <0    
